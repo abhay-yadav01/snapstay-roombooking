481cb4d99c96a48e90b3e7d0c7c7648419f634f3
abhay-yadav01 <abhayyadav19110@gmail.com>
2024-01-30 11:26:37 +0530

    upload on github

--- snapstay-booking/src/main.jsx
+++ snapstay-booking/src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom";
+import App from "./App";
+import { createGlobalStyle } from "styled-components";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap');
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html,
+  body {
+    font-family: 'Oswald', sans-serif;
+    font-size: 62.5%;
+
+    @media (max-width: 768px) {
+      font-size: 50%;
+    }
+    @media (max-width: 450px) {
+      font-size: 40%;
+    }
+  }
+
+  body {
+    font-size: 1.6rem;
+  }
+`;
+
+const root = document.getElementById("root");
+const rootElement = createRoot(root);
+
+rootElement.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
